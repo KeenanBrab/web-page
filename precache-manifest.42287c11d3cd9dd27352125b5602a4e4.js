1482d6d51f980fca3ad73286e27d94c7d86e4f62
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-19 21:32:08 +0000

    Deploy KeenanBrab/web-page to github.com/KeenanBrab/web-page.git:gh-pages

---
+++ precache-manifest.42287c11d3cd9dd27352125b5602a4e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df95361b52883b5e88d76b4754b8be76",
+    "url": "/web-page/index.html"
+  },
+  {
+    "revision": "ccabd5c43c974b3fbe3d",
+    "url": "/web-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "339ffaf3e296531035c9",
+    "url": "/web-page/static/js/2.ac155129.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web-page/static/js/2.ac155129.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccabd5c43c974b3fbe3d",
+    "url": "/web-page/static/js/main.ffecfc69.chunk.js"
+  },
+  {
+    "revision": "013cc42fd98ad2efc779",
+    "url": "/web-page/static/js/runtime-main.22049b66.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/web-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
